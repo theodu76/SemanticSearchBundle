787dc7247b7dbad3bf553265e8990461d40f58dc
theodu76 <lecointretheo@gmail.com>
2018-10-23 09:04:19 +0200

    structure service and delete second service

--- src/iDCity/SemanticSearchBundle/getArticles/getArticles.php
+++ src/iDCity/SemanticSearchBundle/getArticles/getArticles.php
@@ -1,13 +1,43 @@
 <?php 
 
-namespace iDCity\SemanticSearchBundle\getArticles;
+namespace iDCity\SemanticSearchBundle\GetArticles;
 
 class GetArticles{
 
     public function get($listSynonyms){
 
+        foreach ($listSynonyms as $synonym){
 
-        return $listArticles;
+            //Requête permettant de récupérer la liste d'articles contenant synonym
+            //(on prend les articles en entier)
+
+
+            //ajout de la liste d'articles concernés dans le tableau liste article 
+
+        }
+        //appel de la fct classify pour classer les articles par score décroissant
+            //on stocke le résultat dans le tableau $arrayArticles = [for i [ article[i], score[article[i]]]
+            $arrayArticles =  classify($listArticles);
+
+
+            //fonction de tri des articles pour renvoyer le mm tableau mais trié
+            $arrayArticlesSorted = sort($arrayArticles);
+
+
+        return $arrayArticlesSorted;
+    }
+
+
+    public function classify($listArticles){
+
+
+        return $arrayArticles;
+    }
+
+    public function sort($arrayArticles){
+
+
+        return $arrayArticlesSorted;
     }
 
 }
